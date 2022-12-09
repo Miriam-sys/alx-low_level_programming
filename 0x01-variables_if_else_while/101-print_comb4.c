ce43ef37272fd7f2964cc114976ca32f303aadb2
Miriam <miriameke5@gmail.com>
2022-12-09 01:55:18 -0800

    Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int units = '0';
+	int tens = '0';
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (units = '0'; units <= '9'; units++)
+			{
+				if (!((units == tens) || (tens == hundreds) || (tens > units) || (hundreds > tens)))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(units);
+					if (!(units == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
