18e7dc1e6c12dd6ccc6643de4eb2def0b3edc70d
Miriam <miriameke5@gmail.com>
2022-12-09 06:59:26 -0800

    Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,31 +8,30 @@
 
 int main(void)
 {
-	int units = '0';
-	int tens = '0';
-	int hundreds = '0';
-
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
-	{
-		for (tens = '0'; tens <= '9'; tens++)
-		{
-			for (units = '0'; units <= '9'; units++)
-			{
-				if (!((units == tens) || (tens == hundreds) ||
-						       	(tens > units) || (hundreds > tens)))
-				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(units);
-					if (!(units == '9' && hundreds == '7' && tens == '8'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int units = '0';
+int tens = '0';
+int hundreds = '0';
+ for (hundreds = '0'; hundreds <= '9'; hundreds++)
+   {
+     for (tens = '0'; tens <= '9'; tens++)
+       {
+	 for (units = '0'; units <= '9'; units++)
+	   {
+	     if (!((units == tens) || (tens == hundreds) ||\
+		   (tens > units) || (hundreds > units)))
+	       {
+		 putchar(hundreds);
+		 putchar(tens);
+		 putchar(units);
+		 if (!(units == '9' && hundreds == '7' && tens == '8'))
+		   {
+		     putchar(',');
+		     putchar(' ');
+		   }
+	       }
+	   }
+       }
+   }
+ putchar('\n');
+ return (0);
 }
